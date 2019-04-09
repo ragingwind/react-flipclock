6b224612168b64372492ec128e08b3eb37faa860
Jimmy Moon <ragingwind@gmail.com>
2019-04-10 06:39:23 +0900

    Setup DE

---
+++ demo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Flipclock from '../packages/flipclock/src/flipclock'
+
+const App = (<div><Flipclock/></div>)
+
+ReactDOM.render(App, document.getElementById('app'));
---
+++ packages/flipclock/src/flipclock.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Flipclock extends Component {
+  constructor() {
+    super()
+    console.log('init');
+  }
+  render() {
+    return (<div>Hello</div>);
+  }
+}
+
+export default Flipclock
