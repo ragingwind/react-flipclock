fd483837f127571f558c7ff5733ec4efb50754f9
Jimmy Moon <ragingwind@gmail.com>
2019-04-10 21:04:36 +0900

    Set layout

--- demo/index.js
+++ demo/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Flipclock from '../packages/flipclock/src/flipclock'
 
-const App = (<div><Flipclock/></div>)
+const App = (<div style={{width: 640, height: 460}}><Flipclock/></div>)
 
 ReactDOM.render(App, document.getElementById('app'));
--- packages/flipclock/src/flipclock.jsx
+++ packages/flipclock/src/flipclock.jsx
@@ -1,13 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-class Flipclock extends Component {
-  constructor() {
-    super()
-    console.log('init');
-  }
+const styles = {
+  container: {
+    display: 'flex',
+    backgroundColor: 'grey'
+  },
+  flips: {
+    display: 'flex',
+    flexDirection: 'column',
+    border: '1px solid green',
+    width: '33.3%',
+    height: '100%',
+    backgroundColor: 'blue'
+  },
+  flip: {
+    border: '1px solid blue',
+    width: '100%',
+    height: '50%',
+    backgroundColor: 'red'
+  },
+};
+
+const Flip = (props) => {
+  return (
+    <div style={styles.flip}>{props.side}</div>
+  )
+}
+
+class Flips extends Component {
   render() {
-    return (<div>Hello</div>);
+    return (
+      <div style={styles.flips}>
+        <Flip side={"upside"}></Flip>
+        <Flip side={"downside"}></Flip>
+      </div>
+    )
   }
 }
 
-export default Flipclock
+class Flipclock extends Component {
+  render() {
+    return (
+      <div style={styles.container}>
+        <Flips></Flips>
+        <Flips></Flips>
+        <Flips></Flips>
+      </div>
+    );
+  }
+}
+
+export default Flipclock;
